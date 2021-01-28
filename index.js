0d06f5b2e0fbcc952b2e950ca03c2e391644a89a
Federico Benitez <fedebenitez1696@gmail.com>
2021-01-28 20:38:59 -0300

    resolvers y modelos en archivos separados

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
-const typeDefs = gql`
-  type Query {
-    sayHi: String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    sayHi: () => "Hola Mundo"
-  }
-};
 
 const server = new ApolloServer({
   typeDefs,
@@ -23,10 +13,10 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("DB is connected");
     return server.listen({ port: 5000 });
   })
   .then((res) => {
+    console.log("DB is connected");
     console.log(`Server running at ${res.url}`);
   })
   .catch((err) => {
