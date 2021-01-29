d850e3e13fe497907b0f2a2a3c1dd58d7a0b66bb
Federico Benitez <fedebenitez1696@gmail.com>
2021-01-29 10:54:13 -0300

    Creacion y eliminacion de post agregada

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,9 +1,13 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
+
 module.exports = {
   Query: {
     async getAllPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 }); //obtener los ultimos primero
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -16,6 +20,37 @@ module.exports = {
           return post;
         } else {
           throw new Error("post not found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      console.log(user);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString()
+      });
+
+      const post = await newPost.save();
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post eliminado";
+        } else {
+          //el usuario que intenta eliminar el post no es el creador
+          throw new AuthenticationError("Accion no valida");
         }
       } catch (err) {
         throw new Error(err);
