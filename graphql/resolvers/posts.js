db84167c9f02d2f1ce314435c58a300b5510a413
Federico Benitez <fedebenitez1696@gmail.com>
2021-01-29 12:40:55 -0300

    funcionalidad de dar likes agregada

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -55,6 +55,26 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //El post ya tenia un like del usuario, se quita el like
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //Nuevo like
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+        //guardar cambios
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     }
   }
 };
