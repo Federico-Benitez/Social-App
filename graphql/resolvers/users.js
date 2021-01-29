e41f9d177c8b7bef955125aed8c2625efd660137
Federico Benitez <fedebenitez1696@gmail.com>
2021-01-29 09:25:54 -0300

    validaciones implementadas

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
+const { validateRegisterInput } = require("../../util/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
@@ -14,6 +15,15 @@ module.exports = {
       info
     ) {
       //TODO: Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Error", { errors });
+      }
       //TODO: Make sure user doesnt already exist
       const user = await User.findOne({ username });
       if (user) {
